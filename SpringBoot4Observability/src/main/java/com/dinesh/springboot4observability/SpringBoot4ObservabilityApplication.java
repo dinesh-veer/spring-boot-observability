d7075c75bee30f283b0d2613c707ae4e2d31fdb3
dinesh-veer <dveer123@hotmail.com>
2026-01-26 07:10:40 +0530

    Spring boot 4 observability changes.

---
+++ SpringBoot4Observability/src/main/java/com/dinesh/springboot4observability/SpringBoot4ObservabilityApplication.java
@@ -0,0 +1,13 @@
+package com.dinesh.springboot4observability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot4ObservabilityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot4ObservabilityApplication.class, args);
+    }
+
+}
