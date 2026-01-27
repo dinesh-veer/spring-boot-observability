8f6df7033c3f4abc8d8578174654119d1c1b961b
dinesh-veer <dveer123@hotmail.com>
2026-01-27 21:30:31 +0530

    Spring boot 4 observability changes.

---
+++ SpringBoot4Observability/src/test/java/com/dinesh/springboot4observability/SpringBoot4ObservabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinesh.springboot4observability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot4ObservabilityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
